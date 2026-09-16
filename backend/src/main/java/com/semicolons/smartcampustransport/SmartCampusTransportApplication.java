f4517f544bf9c90e18b962188fefb93a53fa743c
StarkBuilds <arghadeeptambuli05@gmail.com>
2026-09-16 11:28:36 +0530

    Initializing Project

---
+++ backend/src/main/java/com/semicolons/smartcampustransport/SmartCampusTransportApplication.java
@@ -0,0 +1,13 @@
+package com.semicolons.smartcampustransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusTransportApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/semicolons/smartcampustransport/SmartCampusTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolons.smartcampustransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
